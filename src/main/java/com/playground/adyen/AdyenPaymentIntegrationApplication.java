effe748ca30943158dc6d924c425dafead0bf81c
Mikhail Shumenko <mykhailo.shumenko@auto1.com>
2021-01-14 16:19:59 +0200

    Disable Consul

--- src/main/java/com/playground/adyen/AdyenPaymentIntegrationApplication.java
+++ src/main/java/com/playground/adyen/AdyenPaymentIntegrationApplication.java
@@ -2,14 +2,13 @@ package com.playground.adyen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @EnableJpaRepositories
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class AdyenPaymentIntegrationApplication {
